6a31b0dea0793821ca74b3a005e893864af777f6
sametmarasli <samet.marasli@gmail.com>
2023-06-29 17:00:39 +0000

    add the dataclass models,revise models

---
+++ flows/main.py
@@ -0,0 +1,24 @@
+from typing import List
+
+from src.callapi import get_endpoint
+from src.models import ApiParameters, ApiResponse, AdvertSchema
+
+
+ENDPOINT = "character"
+
+
+def get_all_paginated_results(pages: int, params: ApiParameters) -> List[AdvertSchema]:
+    results = []
+    for page in range(1, pages + 1):
+        params.pag = page
+        print(f"Calling page {page}")
+        response = get_endpoint(params)
+        results.extend(response.results)
+    return results
+
+
+if __name__ == "__main__":
+    params = ApiParameters(prezzoMinimo=90_000,prezzoMassimo=100_000)
+    response = get_endpoint(params)
+    results = get_all_paginated_results(response.maxPages, params)
+    print(f"Total records: {len(results)}")
